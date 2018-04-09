7f043190ebc6eb72e828b88842f3f2b4b5cd13b2
Valentin <valentin.zander@gmail.com>
2018-04-09 15:16:30 +0200

    cameraScrolling & file archy

---
+++ Assets/Scripts/CameraControler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControler : MonoBehaviour {
+
+
+    #region mainControle
+
+    private GameObject m_target = null;
+
+    [SerializeField]
+    private float m_cameraSpeed = 1.0f;
+
+    public float CameraSpeed { get { return m_cameraSpeed; } set { m_cameraSpeed = value; } }
+
+
+	void Update () {
+        UpdateCam();
+	}
+
+    private void UpdateCam()
+    {
+        Vector3 pos = transform.position;
+        pos.x += m_cameraSpeed * Time.deltaTime;
+        transform.position = pos;
+    }
+    #endregion
+}
