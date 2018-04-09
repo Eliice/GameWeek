f813466e35724566618a7a02cdc16cf1b3210a62
Valentin <valentin.zander@gmail.com>
2018-04-09 16:28:25 +0200

    scrolling
    dead zone

--- Assets/Scripts/Scrolling.cs
+++ Assets/Scripts/Scrolling.cs
@@ -1,9 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class CameraControler : MonoBehaviour {
-
+public class Scrolling : MonoBehaviour {
 
     #region mainControle
 
@@ -13,7 +10,6 @@ public class CameraControler : MonoBehaviour {
     private float m_cameraSpeed = 1.0f;
 
     public float CameraSpeed { get { return m_cameraSpeed; } set { m_cameraSpeed = value; } }
-
 
 	void Update () {
         UpdateCam();
@@ -26,4 +22,14 @@ public class CameraControler : MonoBehaviour {
         transform.position = pos;
     }
     #endregion
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            /// Trigger GameOver & retry screen
+        }
+    }
+
 }
