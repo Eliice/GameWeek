ae48c41c4dbc690139065aca3dd9741c89f5df84
Valentin <valentin.zander@gmail.com>
2018-04-09 17:05:26 +0200

    base main screen

---
+++ Assets/Scripts/StartScreen/NextScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextScene : MonoBehaviour {
+	
+	void Update () {
+		if(Input.anyKey)
+        {
+            Debug.Log("go to next");
+        }
+	}
+}
