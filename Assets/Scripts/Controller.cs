c15a70050cff050523e369bd17989b04306ba478
Valentin GDP2 <v.aguas@student.isartdigital.com>
2018-04-10 10:31:50 +0200

    Player Dash

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -27,27 +27,4 @@ public class Controller : MonoBehaviour
     {
 
     }
-
-    protected IEnumerator Jump()
-    {
-        float force = m_player.Force;
-        m_player.Force = 0;
-        Vector3 initalPos = transform.position;
-        Vector3 pos;
-        do
-        {
-            pos = gameObject.transform.position;
-            pos.y += force * Time.deltaTime;
-            transform.position = pos;
-            force -= m_player.Gravity * Time.deltaTime;
-            yield return new WaitForEndOfFrame();
-        } while (initalPos.y < transform.position.y);
-
-        if (transform.position.y < initalPos.y)
-        {
-            pos.y = initalPos.y;
-            transform.position = pos;
-        }
-        m_player.CanJump = true;
-    }
 }
