67ae7f622bf1eaceaa0f592f3c422e0f0579587c
Valentin <valentin.zander@gmail.com>
2018-04-10 12:06:01 +0200

    UiControler

---
+++ Assets/Scripts/StartScreen/UIControler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControler : MonoBehaviour {
+
+    private bool isStartingScreen = true;
+
+    [SerializeField]
+    private GameObject m_startingScreen = null;
+    [SerializeField]
+    private GameObject m_menuScreen = null;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isStartingScreen)
+            return;
+        if (Input.anyKey)
+        {
+            SwitchScreen();
+        }
+    }
+
+    private void SwitchScreen()
+    {
+        m_startingScreen.SetActive(false);
+        m_menuScreen.SetActive(true);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
