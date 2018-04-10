6f772bedc1c522d9d91a16953f7ad6c084e249e7
Valentin GDP2 <v.aguas@student.isartdigital.com>
2018-04-10 11:13:05 +0200

    Input Controller Pad + Dash Factor

--- Assets/Scripts/ControllerPad.cs
+++ Assets/Scripts/ControllerPad.cs
@@ -2,15 +2,79 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ControllerPad : MonoBehaviour {
+public class ControllerPad : Controller {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private static ControllerPad instance;
+
+    #region inputs
+    private const string horizontalAxis = "Horizontal";
+    private const string A_Button = "A Button";
+    private const string B_Button = "B Button";
+    private const string X_Button = "X Button";
+    private const string Y_Button = "Y Button";
+    #endregion 
+
+    /// instance unique de la classe    
+    public static ControllerPad Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    protected void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else 
+            instance = this;
+
+        m_player = GetComponent<Player>();
+    }
+
+    override protected void Move()
+    {
+        if (Input.GetAxis(horizontalAxis) < -0.25f)
+            m_player.MoveHorizontal(false);
+
+        else if (Input.GetAxis(horizontalAxis) > 0.25f)
+            m_player.MoveHorizontal(true);
+
+        else
+            m_player.Stand();
+    }
+
+    override protected void checkJump()
+    {
+        if (m_player.CanJump && (Input.GetButton(A_Button) || Input.GetButton(B_Button)))
+        {
+            m_player.AddForce();
+        }
+
+        if (Input.GetButtonUp(A_Button) || Input.GetButtonUp(B_Button))
+        {
+            m_player.Jump();
+        }
+    }
+
+    override protected void checkDash()
+    {
+        if (m_player.CanDash && (Input.GetButton(X_Button) || Input.GetButton(Y_Button)))
+        {
+            m_player.AddForce();
+        }
+
+        if (Input.GetButtonUp(X_Button) || Input.GetButtonUp(Y_Button))
+        {
+            m_player.Dash();
+        }
+    }
+
+    protected void OnDestroy()
+    {
+        instance = null;
+    }
 }
