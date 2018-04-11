af1985c9d299927cfec7570526857d83b85aac72
PoNo <lopzerk@hotmail.fr>
2018-04-11 21:38:11 +0200

    Menu UI Animation etc

---
+++ Assets/Scripts/UI/Transitions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transitions : MonoBehaviour {
+
+	private UIControler Controler;
+	private GameMng GM;
+
+	public Animator Animator;
+
+	void Awake () {
+		Animator = this.GetComponent<Animator>();
+
+		if (this.name == "Transitions")
+			DontDestroyOnLoad(this);
+
+		GameObject Manager = GameObject.Find("ManagerHolder");
+		GM = Manager.GetComponent<GameMng>();
+		Controler = Manager.GetComponent<UIControler>();
+	}
+	public void LoadingScreen()
+	{
+		Controler.LoadPlayScene();
+	}
+	public void Respawn()
+	{
+		GM.SpawnPlayer();
+	}
+	public void SwitchScreen()
+	{
+		Controler.SwitchScreen();
+	}
+}
