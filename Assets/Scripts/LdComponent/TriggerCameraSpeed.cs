b9365c51904ea2dfee689de1d2d45f2595047e36
Valentin <valentin.zander@gmail.com>
2018-04-11 10:59:25 +0200

    Camera Trigger

---
+++ Assets/Scripts/LdComponent/TriggerCameraSpeed.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class TriggerCameraSpeed : MonoBehaviour {
+
+
+    [SerializeField]
+    private float m_newCameraSpeed = 1f;
+
+
+    private Scrolling m_CameraScrolling = null;
+
+	void Start () {
+        m_CameraScrolling = Camera.main.GetComponent<Scrolling>();
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            m_CameraScrolling.CameraSpeed = m_newCameraSpeed;
+    }
+
+}
+
