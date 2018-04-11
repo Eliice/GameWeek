e8dd3334e77363b638e0fe56c4a096cee0e41001
Valentin <valentin.zander@gmail.com>
2018-04-11 12:32:33 +0200

    Obstacle script

---
+++ Assets/Scripts/LdComponent/OneShootObstacle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class OneShootObstacle : MonoBehaviour {
+
+    [SerializeField]
+    private float m_speed = 0f;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            GameMng.Instance.SpawnPlayer();
+        }
+    }
+
+    private void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x += m_speed * Time.deltaTime;
+        transform.position = pos;
+    }
+
+}
