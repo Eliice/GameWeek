a3433a42d145f3d7834508155d7166b18aa0ee88
Valentin <valentin.zander@gmail.com>
2018-04-11 15:00:40 +0200

    ui hide on load

--- Assets/Scripts/UI/DelaySpawn.cs
+++ Assets/Scripts/UI/DelaySpawn.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class DelaySpawn : MonoBehaviour {
 
+    [SerializeField]
+    private float m_delayBeforSpawn = 2.9f;
+
+
 	// Use this for initialization
 	void Start () {
         transform.Rotate(0, 90, 0);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
+
+
+
+
