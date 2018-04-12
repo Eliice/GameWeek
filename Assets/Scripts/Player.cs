7c17a2394e3dfa6f9257cfd94ed1837fe4373b28
Valentin <valentin.zander@gmail.com>
2018-04-12 10:12:33 +0200

    first sound integration

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -192,6 +192,7 @@ public class Player : MonoBehaviour
         else
             canGoForward = true;
 
+        SoundCheck();
         //Debug.Log("Forward : " + canGoForward);
     }
 
@@ -324,12 +325,15 @@ public class Player : MonoBehaviour
     private void SoundCheck()
     {
         string name = m_renderer.sprite.name;
+        Debug.Log("in checkSound");
         if (m_frameName.Contains(name))
         {
+            Debug.Log("name check");
             for (int i = 0; i < m_frameName.Count; i++)
             {
                 if (m_frameName[i] == name)
                 {
+                    Debug.Log("sound located");
                     m_audioPlayer.PlayOneShot(m_audioData[i]);
                 }
             }
